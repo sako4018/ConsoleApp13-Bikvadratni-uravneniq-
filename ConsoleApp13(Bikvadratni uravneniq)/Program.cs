6e606e7d575debac1df89ed61000be7ce9248bf6
Sarkis Demirdjian <sarkdemi@gmail.com>
2026-07-25 23:26:59 +0300

    Add IsPrime method to math problems
    - Added IsPrime(int number) method that checks if a number is prime
    - Efficient algorithm using 6k ± 1 optimization
    - Handles edge cases (numbers <= 1)

--- ConsoleApp13(Bikvadratni uravneniq)/Program.cs
+++ ConsoleApp13(Bikvadratni uravneniq)/Program.cs
@@ -79,5 +79,24 @@ namespace ConsoleApp13_Bikvadratni_uravneniq_
             }
             return result;
         }
+
+        // Method to check if a number is prime
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number <= 3)
+                return true;
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
